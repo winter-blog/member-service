4540a6e8c6d1bcf5f29ce93a67be313a1d1a4247
Lee Dong Hun <109887384+donghun93@users.noreply.github.com>
2023-02-24 11:22:01 +0900

    Fix: 비밀번호 변경 시 일치 여부 버그 수정 (#20)
    Resolvers: #19

--- src/main/java/com/devwinter/memberservice/application/service/EditPasswordMemberService.java
+++ src/main/java/com/devwinter/memberservice/application/service/EditPasswordMemberService.java
@@ -4,11 +4,14 @@ import com.devwinter.memberservice.application.port.input.EditPasswordMemberUseC
 import com.devwinter.memberservice.application.port.output.LoadMemberPort;
 import com.devwinter.memberservice.application.port.output.UpdatePasswordMemberPort;
 import com.devwinter.memberservice.application.port.output.history.MemberPasswordEditHistoryPort;
+import com.devwinter.memberservice.application.service.exception.MemberException;
 import com.devwinter.memberservice.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.devwinter.memberservice.application.service.exception.MemberErrorCode.MEMBER_PASSWORD_SAME;
 
 @Service
 @RequiredArgsConstructor
@@ -25,9 +28,11 @@ public class EditPasswordMemberService implements EditPasswordMemberUseCase {
         Member member = loadMemberPort.findById(command.memberId());
 
         String originalPassword = member.getPassword();
-        String changePassword = passwordEncoder.encode(command.changePassword());
 
-        member.changePassword(changePassword);
+        if(passwordEncoder.matches(command.changePassword(), member.getPassword())) {
+            throw new MemberException(MEMBER_PASSWORD_SAME);
+        }
+        member.changePassword(passwordEncoder.encode(command.changePassword()));
         updatePasswordMemberPort.updatePassword(member);
         // TODO: 변경 이력 (카프카 - 커넥터 사용)
         // memberPasswordEditHistoryPort.send(member, originalPassword);
