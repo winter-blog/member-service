3409b30f7048a1c1afb1bcb3c6b207386c521bdb
Lee Dong Hun <109887384+donghun93@users.noreply.github.com>
2023-03-10 12:25:24 +0900

    Feature/#31 password change event message kafka (#32)
    * Remove: KafkaProducer Config 제거
    
    
    Resolvers: #31
    
    * Feat: 비밀번호 변경 Domain Event 생성 및 발행
    
    
    Resolvers: #31
    
    * Feat: 메시징 처리 스레드 풀 생성 및 Config 추가
    
    
    Resolvers: #31
    
    * Feat: 비밀번호 변경 Event Handler 개발
    
    
    Resolvers: #31

--- src/main/java/com/devwinter/memberservice/application/service/EditPasswordMemberService.java
+++ src/main/java/com/devwinter/memberservice/application/service/EditPasswordMemberService.java
@@ -2,17 +2,20 @@ package com.devwinter.memberservice.application.service;
 
 import com.devwinter.memberservice.application.port.input.EditPasswordMemberUseCase;
 import com.devwinter.memberservice.application.port.output.LoadMemberQueryPort;
+import com.devwinter.memberservice.application.port.output.PasswordChangeEventPort;
 import com.devwinter.memberservice.application.port.output.UpdatePasswordMemberPort;
 import com.devwinter.memberservice.application.port.output.history.MemberPasswordEditHistoryPort;
 import com.devwinter.memberservice.application.service.exception.MemberException;
 import com.devwinter.memberservice.domain.Member;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.devwinter.memberservice.application.service.exception.MemberErrorCode.MEMBER_PASSWORD_SAME;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EditPasswordMemberService implements EditPasswordMemberUseCase {
@@ -27,13 +30,14 @@ public class EditPasswordMemberService implements EditPasswordMemberUseCase {
     public void editPassword(EditPasswordMemberCommand command) {
         Member member = loadMemberQueryPort.findByMemberId(command.memberId());
 
-        String originalPassword = member.getPassword();
+        // String originalPassword = member.getPassword();
 
         if(passwordEncoder.matches(command.changePassword(), member.getPassword())) {
             throw new MemberException(MEMBER_PASSWORD_SAME);
         }
         member.changePassword(passwordEncoder.encode(command.changePassword()));
         updatePasswordMemberPort.updatePassword(member);
+
         // TODO: 변경 이력 (카프카 - 커넥터 사용)
         // memberPasswordEditHistoryPort.send(member, originalPassword);
     }
