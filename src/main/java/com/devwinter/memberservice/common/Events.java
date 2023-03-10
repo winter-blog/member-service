d3f8d9e1c699f721cee0bc50ae1c8f40f23b087b
donghun93 <alswn4516@gmail.com>
2023-03-10 12:23:58 +0900

    Feat: 메시징 처리 스레드 풀 생성 및 Config 추가
    Resolvers: #31

---
+++ src/main/java/com/devwinter/memberservice/common/Events.java
@@ -0,0 +1,16 @@
+package com.devwinter.memberservice.common;
+
+import org.springframework.context.ApplicationEventPublisher;
+
+public class Events {
+    private static ApplicationEventPublisher publisher;
+
+    public static void setPublisher(ApplicationEventPublisher publisher) {
+        Events.publisher = publisher;
+    }
+    public static void raise(Object event) {
+        if(publisher != null) {
+            publisher.publishEvent(event);
+        }
+    }
+}
---
+++ src/main/java/com/devwinter/memberservice/config/AsyncConfig.java
@@ -0,0 +1,18 @@
+package com.devwinter.memberservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean(name = "messagingTaskExecutor", destroyMethod = "shutdown")
+    public ThreadPoolTaskExecutor messagingTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(200);
+        executor.setMaxPoolSize(200);
+        return executor;
+    }
+}
